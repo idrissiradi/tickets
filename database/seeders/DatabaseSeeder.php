a8bc77e3df876ba288e5af57886d51c7330b3847
idradi <idrissiradi@gmail.com>
2025-09-20 22:29:28 +0100

    feat: Initialize API routes and authentication
    - Created api.php for authentication routes including login, register, and logout.
    - Established api_v1.php for ticket and author management with appropriate resource routes.

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Ticket;
+use App\Models\User;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        $users = User::factory(10)->create();
+
+        Ticket::factory(100)->recycle($users)->create();
+
+        User::create([
+            'email' => 'manager@manager.com',
+            'password' => bcrypt('password'),
+            'name' => 'The manager',
+            'is_manager' => true
+        ]);
+
+        // User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+    }
+}
